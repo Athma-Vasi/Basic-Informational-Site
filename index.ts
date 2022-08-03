8a304da7bdf7712fa2b51dbc4655b9a26b8fac63
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-08-03 11:52:22 -0600

    rewrite server using Deno native http bindings

--- index.ts
+++ index.ts
@@ -1,3 +1,56 @@
+/* 
+USING DENO NATIVE HTTP SERVER 
+*/
+
+import { serve } from 'https://deno.land/std@0.150.0/http/server.ts';
+
+const PORT = 8080;
+
+await serve(handler, { port: PORT });
+
+async function handler(req: Request): Promise<Response> {
+  const url = new URL(req.url);
+
+  const pathWithMethod = `${req.method} ${url.pathname}`;
+
+  switch (pathWithMethod) {
+    case 'GET /': {
+      const data = await Deno.readFile('./index.html');
+      return new Response(data, { status: 200 });
+    }
+
+    case 'GET /index.html': {
+      const data = await Deno.readFile('./index.html');
+      return new Response(data, { status: 200 });
+    }
+
+    case 'GET /about.html': {
+      const data = await Deno.readFile('./about.html');
+      return new Response(data, { status: 200 });
+    }
+
+    case 'GET /contact.html': {
+      const data = await Deno.readFile('./contact.html');
+      return new Response(data, { status: 200 });
+    }
+
+    default: {
+      const data = await Deno.readFile('./404.html');
+      return new Response(data, { status: 404 });
+    }
+  }
+}
+
+//
+//
+//
+//
+//
+//
+//
+/*
+USING EXPRESS WITH NODEJS
+
 import express, { Application, Request, Response } from 'express';
 import { readFile } from 'fs/promises';
 
@@ -27,3 +80,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+*/
